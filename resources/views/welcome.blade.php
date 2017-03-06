26a87bfe0cbb712b9808399e86a5cfe540069e66
Stanislav Mõškovski <stanislav.myshkovski@gmail.com>
2017-03-06 22:16:36 +0200

    Lisa kuulutust leht ja lisasin avalehel nupu kuulutuse lisamiseks.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,71 +11,15 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="/LAFF/public/css/default.css" rel="stylesheet" type="text/css">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+            <div class="top-right links">
+                <a href={{url('/')}}>Home</a>
+                <a href="{{url('/lisa')}}">Lisa kuulutus</a>
+            </div>
 
             <div class="content">
                 <div class="title m-b-md">
@@ -83,7 +27,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">This</a>
+                    <a href="{{url('hello')}}">This</a>
                     <a href="https://laracasts.com">is</a>
                     <a href="https://laravel-news.com">a</a>
                     <a href="https://forge.laravel.com">beta</a>
