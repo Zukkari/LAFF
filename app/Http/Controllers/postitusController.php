67e8f08d2199c7b2414d89975e5071b62452482c
edgarp95 <edgarpasenkov@gmail.com>
2017-03-13 19:16:52 +0200

    Katsetan, kas mu nimi nüüd ka õigesti ilmub...

--- app/Http/Controllers/postitusController.php
+++ app/Http/Controllers/postitusController.php
@@ -6,8 +6,8 @@ class postitusController extends Controller
 {
     public function index()
     {
-        $postitus =DB::select('CALL postitus()');
-        $postitusi= DB::select('CALL postituste_arv()');
+        $postitus =DB::select('CALL postitus()'); //annab meile postituse informatsiooni
+        $postitusi= DB::select('CALL postituste_arv()'); //mitu postitust on süsteemis
 
         return view("postitus", ['postitus'=>$postitus] ,[ 'postitusi'=>$postitusi]);
     }
