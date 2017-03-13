ac7685b0df6efabc0ecf478a715b0a4307080ddc
edgarp95 <edgarpasenkov@gmail.com>
2017-03-13 22:27:38 +0200

    Lisatud võimalus laadida pilte ülesse koos postitusega (samas võimalus pilti mitte lisada säilis)

--- app/Http/Controllers/lisaController.php
+++ app/Http/Controllers/lisaController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Validator;
+use Redirect;
+use Illuminate\Support\Collection;
 class lisaController extends Controller
 {
 
@@ -29,17 +34,38 @@ class lisaController extends Controller
         $tekst=$req->input('tekst');
         $tagid=$req->input('tagid');
         $pildilink=$req->input('pildilink');
-        if (empty($pildilink)){ //Kui pidlilinki pole lisatud, siis näitame pilti, et pilti pole :D
+        $file = array('kuulutusePilt' => Input::file('kuulutusePilt'));
+
+        $arv =count($file);
+        if (empty($file['kuulutusePilt'])) {
             DB::select('CALL lisa_postitus (?,?,?,?,?)',array("noname", $teema, $tekst,"http://www.katiehalerealtor.com/elements/images/design/no_image_available.jpg", $tagid));
         }
         else {
-            DB::select('CALL lisa_postitus (?,?,?,?,?)',array("noname", $teema, $tekst,$pildilink, $tagid));
+            $rules = array('kuulutusePilt' => 'image',); //mimes:jpeg,bmp,png and for max size max:10000
+            // doing the validation, passing post data, rules and the messages
+            $validator = Validator::make($file, $rules);
+            if ($validator->fails()) {
+                // send back to the page with the input data and errors
+                return Redirect::to('lisa')->withInput()->withErrors($validator);
+            } else {
+                // checking file is valid.
+                if (Input::file('kuulutusePilt')->isValid()) {
+                    $destinationPath = '/webpages/lostafcsut/public_html/public/pictures'; // upload path
+                    $extension = Input::file('kuulutusePilt')->getClientOriginalExtension(); // getting image extension
+                    $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
+                    Input::file('kuulutusePilt')->move($destinationPath, $fileName); // uploading file to given path
+                    $path='/../public/pictures';
+                    DB::select('CALL lisa_postitus (?,?,?,?,?)',array("noname", $teema, $tekst,$path."/".$fileName, $tagid));
+                    // sending back with message
+                    //Session::flash('success', 'Upload successfully');
+                    //return Redirect::to('lisa');
+                } else {
+                    // sending back with error message.
+                    Session::flash('error', 'uploaded file is not valid');
+                    return Redirect::to('lisa');
+                }
+            }
         }
-
-
-
-
         return view('welcome');
-
     }
 }
