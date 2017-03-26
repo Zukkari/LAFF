927ca0e31c7306b3e3860d6b562d94cd50c860a5
Stanislav Mõškovski <stanislav.myshkovski@gmail.com>
2017-03-26 19:01:17 +0300

    offline funktsionaalsus keeled

--- en/homePageMessages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'home' => 'Home',
-    'addAd' => 'Add ad',
-    'ads'=>'See ads',
-    'us'=>'About us',
-    'slogan' => 'We will find whatever!'
-];
--- et/homePageMessages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'home' => 'Avaleht',
-    'addAd' => 'Lisa kuulutus',
-    'ads'=>'Vaata postitusi',
-    'us'=>'Meist',
-    'slogan' => 'Me leiame ükskõik mida!'
-];
