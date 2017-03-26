47e2f9fb102ed2b9cc703ea364a4b21403ac1076
Stanislav Mõškovski <stanislav.myshkovski@gmail.com>
2017-03-26 23:17:10 +0300

    eemaldasin javascripti loogika .php failidest

--- resources/views/postitus.blade.php
+++ resources/views/postitus.blade.php
@@ -9,13 +9,15 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/../public/css/postitus.css" rel="stylesheet" type="text/css">
+    <link href="/../public/css/postitus.css" rel="stylesheet">
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="./dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/../public/js/checkConnection.js"></script>
+    <script src="/../public/js/fetchPost.js"></script>
 
     <!-- Head icon -->
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/2000px-Magnifying_glass_icon.svg.png">
@@ -59,7 +61,7 @@
 </div>
 <br/>
 <div class="container">
-    <div class="jumbotron" style="background-color: white">
+    <div class="jumbotron">
         <div class="container-fluid">
             <div class="row content">
                 <div class="col-sm col-md-offset-2">
@@ -76,7 +78,7 @@
                             </li>
                         </ul>
                         <?php foreach ($postitusi as $postitusi) {?>
-                        <p><?php echo $postitusi->arv; echo __('adPageMessages.totalAds') ?></p><?php } ?>
+                        <p class="postitusi"><?php echo $postitusi->arv; echo __('adPageMessages.totalAds') ?></p><?php } ?>
                         <div class="posts endless-pagination" data-next-page="{{$postitus->nextPageUrl()}}">
                             @foreach($postitus as $post)
                                 <div class="col-md-12 col-lg-12 container">
@@ -85,26 +87,27 @@
                                         <h5><span class="glyphicon glyphicon-time"></span><?php echo __('adPageMessages.user'); echo $post->kasutaja; echo ", " ; echo $post->date; echo ", "; echo $post->email?></h5>
                                         <h5><span class="label label-danger"><?php echo $post->peatag ?></span> <span class="label label-primary">kaotatud</span></h5><br>
                                         <div>
-                                            <p><img src="<?php echo $post->pildilink ?>" alt="image"></p>
-                                            <p><?php echo $post->kirjeldus ?></p>
+                                            <img class="kuulutusePilt" src="<?php echo $post->pildilink ?>" alt="image">
+                                            <p class="kirjeldus"><?php echo $post->kirjeldus ?></p>
                                         </div>
                                     </div>
                                 </div>
                             @endforeach
+			<hr>
                         </div>
 
 
-                        <div class="loading" style="text-align: center">
-                            <img src="/../public/pictures/waiting.gif" style="width:100px; height: 100px" alt="loadingGIF">
-                        </div>
+                        <div><span class="loading">
+                            <img class="loadimg" src="/../public/pictures/waiting.gif" alt="loadingGIF"/>
+                        </span></div>
 
 
                         <div id="connectionerror" class="alert alert-danger alert-dismissable">
-                            Internetiühendust pole
+                            <?php echo __('warnings.noconnection') ?>
                         </div>
 
                         <div id="connectionestab" class="alert alert-success alert-dismissable">
-                            Internetiühendus taastatud!
+                            <?php echo __('warnings.connection') ?>
                         </div>
 
                         <script>
@@ -114,53 +117,14 @@
                             $('.loading').hide(); //eespool defineeritud loading gif, koguaeg me seda ei näita
                             $(document).ready(function () {
                                 $(window).scroll(fetchPost);
-                                function fetchPost() { //fetchimine siin
-                                    if (navigator.onLine) {
-                                        var page = $('.endless-pagination').data('next-page');
-                                        if (page !== null) { //Kui meil veel midagi fetchida, juurde laadida, näitame laadimise ajal loading gifi ja laeme ajaxi abil juurde
-                                            $('.loading').show();
-                                            clearTimeout($.data(this, "scrollCheck"));
-                                            $.data(this, "scrollCheck", setTimeout(function () {
-                                                var scroll_position_for_post_load = $(window).height() + $(window).scrollTop() + 100;
-                                                if (scroll_position_for_post_load >= $(document).height()) {
-                                                    $.get(page, function (data) {
-                                                        $('.posts').append(data.postitus);
-                                                        $('.endless-pagination').data('next-page', data.next_page);
-
-                                                    });
-                                                    $('.loading').hide(); //Kui info laetud kaotame loading gifi
-                                                }
-                                            }, 350))
-                                        } else { //Kui jõumae lõppu
-                                            $('.loading').hide();
-                                        }
-                                    }
-                                }
-                            })
+                            });
                         </script>
 
                         <script type="text/javascript">
                             document.getElementById('connectionerror').style.display = 'none';
                             document.getElementById('connectionestab').style.display = 'none';
+			    var last = false;
                             window.setInterval(checkConnection, 5000);
-
-                            var last = false;
-                            function checkConnection() {
-                                var current = navigator.onLine;
-
-                                if (current) {
-                                    document.getElementById('connectionerror').style.display = 'none';
-                                    if (last) {
-                                        document.getElementById('connectionestab').style.display = 'block';
-                                        last = false;
-                                    } else {
-                                        document.getElementById('connectionestab').style.display = 'none';
-                                    }
-                                } else {
-                                    document.getElementById('connectionerror').style.display = 'block';
-                                    last = true;
-                                }
-                            }
                         </script>
                     </div>
                 </div>
--- storage/framework/views/a579c5313ed2fe9480c517d67a9b16689051135f.php
+++ storage/framework/views/a579c5313ed2fe9480c517d67a9b16689051135f.php
@@ -7,41 +7,66 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Lost & Found Foundation</title>
+        <title>Lostaf Main</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <link href="/../public/css/default.css" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/../public/css/postitus.css" rel="stylesheet" type="text/css">
+
+        <!-- Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="./dist/js/bootstrap.min.js"></script>
+
+        <!-- Head icon -->
+        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/2000px-Magnifying_glass_icon.svg.png">
 
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-                <div class="top-right links">
-                        <a href=<?php echo e(url('/')); ?>><?php echo __('homePageMessages.home')?></a>
-                        <a href="<?php echo e(url('/postitus')); ?>"><?php echo __('homePageMessages.ads')?></a>
-
-                        <?php echo e(Session::put('redirectTo', '/')); ?>
-
-                        <?php if(auth()->check()): ?>
-                            <a href="<?php echo e(url('/lisa')); ?>"><?php echo __('homePageMessages.addAd')?></a>
-                            <a href="<?php echo e(route('logout')); ?>"><?php echo __('auth.logout')?></a>
-                        <?php else: ?>
-                            <a href='<?php echo e(route('login')); ?>'><?php echo __('auth.login')?></a>
-                            <a href='<?php echo e(route('register')); ?>'><?php echo __('auth.register')?></a>
-                        <?php endif; ?>
-
-                    <div class="position-ref right">
-                        <?php $__currentLoopData = config('app.locales'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $lang => $language): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                            <div class="links"><a href="<?php echo e(route('lang.switch', $lang)); ?>"><img src='<?php echo e(asset('/icons/'.$lang.'.png')); ?>' alt=<?php echo e($language); ?>> <?php echo e($language); ?></a></div>
-                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href=<?php echo e(url('/')); ?>>Lost & Found Foundation</a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li title="<?php echo __('userHelp.home')?>" class="active"><a href='<?php echo e(url('/')); ?>'><span class="glyphicon glyphicon-home"></span><?php echo __('homePageMessages.home')?></a></li>
+                            <li title="<?php echo __('userHelp.seeAds')?>" ><a href="<?php echo e(url('/postitus')); ?>"><?php echo __('homePageMessages.ads')?></a></li>
+                            <li title="<?php echo __('userHelp.aboutUs')?>"><a href="<?php echo e(url('/meist')); ?>"><span class="glyphicon glyphicon-info-sign"></span><?php echo __('homePageMessages.us') ?></a></li>
+                            <?php if(auth()->check()): ?>
+                                <li><a title="<?php echo __('userHelp.addAd')?>" href="<?php echo e(url('/lisa')); ?>"><?php echo __('homePageMessages.addAd')?></a></li>
+                                <li title="<?php echo __('userHelp.logout')?>"><a href="<?php echo e(route('logout')); ?>"><?php echo __('auth.logout')?></a></li>
+                            <?php else: ?>
+                                <li><a title="<?php echo __('userHelp.login')?>" href='<?php echo e(route('login')); ?>'><?php echo __('auth.login')?></a></li>
+                                <li><a title="<?php echo __('userHelp.register')?>" href='<?php echo e(route('register')); ?>'><?php echo __('auth.register')?></a></li>
+                            <?php endif; ?>
+                            <li><form class="navbar-search navbar-form" method="get">
+                                    <input title="<?php echo __('userHelp.search')?>" class="form-control" placeholder="<?php echo __('adPageMessages.search') ?>" name="s" type="text">
+                                </form>
+                            </li>
+                            <li class="menu-item dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('adPageMessages.lang') ?><b class="caret"></b></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><?php $__currentLoopData = config('app.locales'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $lang => $language): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <a href="<?php echo e(route('lang.switch', $lang)); ?>"><img src='<?php echo e(asset('/icons/'.$lang.'.png')); ?>' alt="<?php echo e($language); ?>"> <?php echo e($language); ?></a>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-
+            </div>
                 <div class="content">
                     <div class="title m-b-md">
-                        Lost & Found Foundation
+                        Lost & Found Foundation <br>
+                        <?php echo __('homePageMessages.slogan')?>
                     </div>
                     <?php if(auth()->check()): ?>
                     <div class="title m-b-md">
