0086d0e8a205adb0f60058f719d41e1a5468279a
languidpie <mariliis.pihlapuu@gmail.com>
2017-04-06 14:39:03 +0300

    Profiili ja footeri view

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container">
+            <p class="navbar-text pull-left">© 2017 - Created by Stanislav Mõškovski, Mari-Liis Pihlapuu, Edgar Pašenkov
+            </p>
+        </div>
+    </div>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Lostaf Main</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="/../public/css/default.css" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/../public/css/postitus.css" rel="stylesheet" type="text/css">
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="./dist/js/bootstrap.min.js"></script>
+
+    <!-- Head icon -->
+    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/2000px-Magnifying_glass_icon.svg.png">
+
+</head>
+<body class="body-bottom">
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <a class="navbar-brand" href={{url('/')}}>Lost & Found Foundation</a>
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse navHeaderCollapse">
+            <ul class="nav navbar-nav">
+                <li title="<?php echo __('userHelp.home')?>"><a href='{{url('/')}}'><span class="glyphicon glyphicon-home"></span><?php echo __('homePageMessages.home')?></a></li>
+                <li title="<?php echo __('userHelp.seeAds')?>" ><a href="{{url('/postitus')}}"><?php echo __('homePageMessages.ads')?></a></li>
+                <li title="<?php echo __('userHelp.aboutUs')?>" class="active"><a href="{{url('/meist')}}"><span class="glyphicon glyphicon-info-sign"></span><?php echo __('homePageMessages.us') ?></a></li>
+                @if(auth()->check())
+                    <li><a title="<?php echo __('userHelp.addAd')?>" href="{{url('/lisa')}}"><?php echo __('homePageMessages.addAd')?></a></li>
+                    <li title="<?php echo __('userHelp.logout')?>"><a href="{{route('logout')}}"><?php echo __('auth.logout')?></a></li>
+                @else
+                    <li><a title="<?php echo __('userHelp.login')?>" href='{{ route('login') }}'><?php echo __('auth.login')?></a></li>
+                    <li><a title="<?php echo __('userHelp.register')?>" href='{{route('register')}}'><?php echo __('auth.register')?></a></li>
+                @endif
+                <li><form class="navbar-search navbar-form" method="get">
+                        <input title="<?php echo __('userHelp.search')?>" class="form-control" placeholder="<?php echo __('adPageMessages.search') ?>" name="s" type="text">
+                    </form>
+                </li>
+                <li class="menu-item dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('adPageMessages.lang') ?><b class="caret"></b></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>@foreach (config('app.locales') as $lang => $language)
+                                <a href="{{ route('lang.switch', $lang) }}"><img src='{{asset('/icons/'.$lang.'.png')}}' alt="{{$language}}"> {{$language}}</a>
+                            @endforeach
+                        </li>
+                    </ul>
+                </li>
+                @if(auth()->check())
+                    <li class="menu-item dropdown">
+                        <a href="#" data-toggle="dropdown"><img src="http://www.nochoffen.de/static/img/avatar_placeholder.png" height="25px"></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{url('/lisa')}}"><?php echo __('userHelp.addAd')?></a>
+                                <a href="{{url('/profiil')}}"><?php echo __('userHelp.profile')?></a>
+                                <a href="#"><?php echo __('userHelp.settings')?></a>
+                                <a href="{{route('logout')}}"><?php echo __('userHelp.logout')?></a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="content">
+        <div class="profile">
+            <h1><?php echo auth()->user()->kasutajanimi ?></h1>
+            <img src="https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png">
+        </div>
+	<p> <i class="glyphicon glyphicon-envelope"></i> minuemail@hot.ee </p>
+        <br>
+        <div class="ads">
+            <h2>My ads</h5>
+		<div class="container">
+    <div class="row col-md-6 col-md-offset-3 custyle">
+    <table class="table table-striped custab">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Date added</th>
+            <th>Lost/Found?</th>
+        </tr>
+    </thead>
+            <tr>
+                <td>1</td>
+                <td>Lost my sanity</td>
+                <td>04/04/2017</td>
+                <td>Lost</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Lost my cat Sniffles</td>
+                <td>02/02/2017</td>
+                <td>Found</td>
+            </tr>
+    </table>
+    </div>
+</div>
+        </div>
+    </div>
+</div>
+<footer>
+<?php include('/webpages/lostafcsut/public_html/resources/views/footer.blade.php'); ?>
+</footer>
+</body>
+</html>
