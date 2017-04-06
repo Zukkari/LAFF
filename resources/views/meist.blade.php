359ce127f5552c75af13a4f3c24005b6ec11a7e1
languidpie <mariliis.pihlapuu@gmail.com>
2017-04-06 22:14:42 +0300

    Pangalink

--- resources/views/meist.blade.php
+++ resources/views/meist.blade.php
@@ -96,7 +96,87 @@
 			<td><p>Edgar Pašenkov (projektijuht) - <a href="mailto:edgarpasenkov@gmail.com" target="_top">saada kuri kiri</a></p></td>
 			</tr>
 			</table>
+            <div class="container-fluid"><div class="break"></div></div>
 
+            <?php
+            $private_key = openssl_pkey_get_private(
+                "-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAnkQhCOW55dSqZ47zo9SLkSZNcYCnTEeRS6WGvneK07XuJ5qN
+exmLWazElZOB/oQXw80stU5p1Uy7z72XOZaouvWBCaNTPjAEupd3PpzUYikfWZDM
+G+juxGDS8+lmdWBiEsder5OFT9GknciKTki1j/p6rGR5FqNAZIFyINVCeOfpSNlB
+TnhRSyIOs0wr5Mv5dIyJONiTnxA54Ne5e0/LGtqjrbK2w/2PaGhuuduOS3MRgKIo
+mEZiWniJa20a41nIY8VAANuj6msYSUR5s+AcnFzJRje2GCrML6aOXSc4vY52nSLA
+yB++mMcu01viRbL+sZpfI/TQ8PQopojDMnRkYQIDAQABAoIBADdLVENYh69tsq+F
+uUfG7ZAj0rDSIyE6a+ADD+WhvGYlTPSXQdD9ZrtI6lHb/HHg0rC9EV6y67TzHzcA
+ZUJgicjwF0o3vtCeVDigzK+aeXVmKqbPORCTPEBuF6XSKNLffsRS1ZkAMHZnp1zd
+AL1DF/Qarhtm6wtmtAupUid4esXkq+XNHa2P2U/sSCOHPUNmD2y84Fxo6h8yc9uk
+ZZd+qCdhu6XqNPhwPlzhirzUNEiaELw8nNu3F2YWrXNNcJec9eAU4a3vCquNEI4R
+4k2Q9xiF5qv8X9g/ZXo1T0YC2as6ryT518F1lJZk8jLv0340kfHTwCf4qEZ5I92x
+tJgbYoUCgYEAzVk0cr9FyWfl+Cr7kdPZMiTUDX/NiljEFiRiCJ56wDqKmE0ESiqf
+klT8HVvqnT62pnlbeeoxKxKON05kJ7bnozBiKHr8bozghqOav3W2aMFBqFa0bwo1
+AiUj3Sv1Z+w+Y3Ifz+KBFCJ84nfSB7K/0/DrcpWuhxMGPYuBocMXJJ8CgYEAxU3l
+MKZHfAcVfgKaXKfez+lWX45RvzmRTTQfL0gj0CUEpI3z7Gsn3bDYflSvMvBJ9aE3
+cTeUznvqPIkHV1EZvy2cBpNKqFPwsMLOQAM0gbeQLNgW0T/yCSA5tizSVnfukMYK
+Xiv8comXzlrHJtX8yq9yRR6gzZr29altc4v21v8CgYEAsS1ApakHK6nrsF5VxRMG
+mc9Q73zP+YhxV7F4rHsg6m8YWiRJiTyRhg6xoRtHqPkNW0HmfaBlYAaYknueyu1z
+m0gJyWekGjaPG1xaDswf4O6uGfMFp7Ek337wHMrq6QnbIq67aADE4nyHSFed7mp6
+PQHoBGvFtGHjxoJyDQJpG30CgYB2umoZfWX015pySoSzDv2AZWxpcg31IWIzcBL3
+89RBM5V4pEdNVCoYOIv+cV1ALOKEoe/n/Edafay1osu9GnuA1KRYPJgndOOYqpFM
+aeFV0a07nwkUAAPYxACWhKBKEj+H9PRWOOff0LZ+IZhH4WsOWMdNmqGvN/o2WC8Z
+4fa25wKBgBqE/S1Fw7ack0aZ9Piu+80DeZtvFnMflVdH5uAqc1GWCgrl7Coc6kO+
+zcmMlFX5A0EthapSgZkQFYDEwSzwkeGgb8RrQLBfPJjaXZR68pDs3/MNq+pHY0xO
+cHmKJCxrpwBgBhb9SEbnMp43yfXArKaI4u4tAqqsCg+hB8vJIKis
+-----END RSA PRIVATE KEY-----");
+
+            $date = date('Y-m-d\TH:i:s\+0300');
+
+            $fields = array(
+                "VK_SERVICE" => "1011",
+                "VK_VERSION" => "008",
+                "VK_SND_ID" => "uid100010",
+                "VK_STAMP" => "12345",
+                "VK_AMOUNT" => "5",
+                "VK_CURR" => "EUR",
+                "VK_ACC" => "EE152200221234567897",
+                "VK_NAME" => "Lost & Found Foundation",
+                "VK_REF" => "1234561",
+                "VK_LANG" => "EST",
+                "VK_MSG" => "Make Lost & Found Foundation great again!",
+                "VK_RETURN" => "http://lostaf.cs.ut.ee/meist",
+                "VK_CANCEL" => "http://lostaf.cs.ut.ee/meist", /* Võib midagi muud ka välja mõelda */
+                "VK_DATETIME" => "2017-04-06T22:05:55+0300",
+                "VK_ENCODING" => "utf-8",
+            );
+
+            $data = str_pad(mb_strlen($fields["VK_SERVICE"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_SERVICE"] .    /* 1011 */
+                str_pad(mb_strlen($fields["VK_VERSION"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_VERSION"] .    /* 008 */
+                str_pad(mb_strlen($fields["VK_SND_ID"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_SND_ID"] .     /* uid100010 */
+                str_pad(mb_strlen($fields["VK_STAMP"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_STAMP"] .      /* 12345 */
+                str_pad(mb_strlen($fields["VK_AMOUNT"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_AMOUNT"] .     /* 5 */
+                str_pad(mb_strlen($fields["VK_CURR"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_CURR"] .       /* EUR */
+                str_pad(mb_strlen($fields["VK_ACC"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_ACC"] .        /* EE152200221234567897 */
+                str_pad(mb_strlen($fields["VK_NAME"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_NAME"] .       /* Lost & Found Foundation */
+                str_pad(mb_strlen($fields["VK_REF"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_REF"] .        /* 1234561 */
+                str_pad(mb_strlen($fields["VK_MSG"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_MSG"] .        /* Make Lost & Found Foundation great again! */
+                str_pad(mb_strlen($fields["VK_RETURN"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_RETURN"] .     /*  */
+                str_pad(mb_strlen($fields["VK_CANCEL"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_CANCEL"] .     /*  */
+                str_pad(mb_strlen($fields["VK_DATETIME"], "UTF-8"), 3, "0", STR_PAD_LEFT) . $fields["VK_DATETIME"];    /* 2016-04-08T18:51:12+0300 */
+
+            openssl_sign($data, $signature, $private_key, OPENSSL_ALGO_SHA1);
+            $fields["VK_MAC"] = base64_encode($signature);
+
+            ?>
+
+            <div class="break"></div>
+            <p>Make Lost & Found Foundation great again!</p>
+
+            <form method="post" action="http://localhost:8080/banklink/swedbank-common">
+
+                <?php foreach($fields as $key => $val):?>
+                <input type="hidden" name="<?php echo $key; ?>" value="<?php echo htmlspecialchars($val); ?>" />
+                <?php endforeach; ?>
+                <input type="submit" class="donate" value="Please take my money"/>
+            </form>
 			
                 <h2 class="meistH">Kust meid leida saab?</h2><br>
                     <div id="map">
